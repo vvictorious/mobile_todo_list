19b3918f4867a8b9f045d7799a96b4831f4c37a7
vvictorious <vvictorious30@gmail.com>
2019-02-19 15:20:35 -0700

    appending list items

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const form = document.getElementById("register");
 const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	console.log(input.value);
+	const text = input.value;
+	const li = document.createElement('li');
+	li.textContent = text;
+	li.className = 'list-li';
+	const label = document.createElement('label');
+	label.textContent = 'finished this yet?';
+	label.className = 'label';
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.className = 'checkbox';
+	li.appendChild(label);
+	li.appendChild(checkbox);
+	ul.appendChild(li);
 });
